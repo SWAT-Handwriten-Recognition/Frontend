a7ce743a8384147fb45cbba0c2d8c3622a8be978
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-26 14:15:02 -0600

    Finish setip

---
+++ src/components/App/index.jsx
@@ -0,0 +1,6 @@
+/**
+ * App component, root of the application.
+ */
+const App = () => <h1>I am the app</h1>
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import { render } from 'react-dom'
 import styled from 'styled-components'
+import App from './components/App/index.jsx'
 
 const root = document.getElementById('root')
 
-const Greet = styled.h2`
+const Greet = styled.div`
   color: red;
 `
 
-render(<Greet>Works</Greet>, root)
+render(<Greet><App /></Greet>, root)
