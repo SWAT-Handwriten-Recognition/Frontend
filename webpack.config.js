7be1b8f7c24788dce38edb9e801b14f75264b08e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-25 20:01:37 -0600

    works

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+//import webpack plugins
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+//css rules
+// const cssRules = {
+//   test: /\.styl$/i,
+//   use: ['style-loader', 'css-loader', 'stylus-loader'],
+//   exclude: /node_modules/,
+// };
+
+//images rules
+// const imgRules = {
+//   test: /\.(jpg|png|svg)$/,
+//   use: {
+//     loader: 'url-loader',
+//     options: {
+//       limit: 90000,
+//     }
+//   }
+// }
+
+//babel rules
+const babelRules = {
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader',
+};
+
+//export config
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.[contentHash].js',
+  },
+  module: {
+    rules: [babelRules],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'Hand Written',
+      template: './public/index.html',
+    }),
+  ]
+};
