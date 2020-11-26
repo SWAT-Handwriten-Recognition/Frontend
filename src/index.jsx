ac789ab357a02b34b3ee97b8803363da40e82936
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-26 17:44:17 -0600

    Finish redux set up

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,28 @@
 import { render } from 'react-dom'
 import styled from 'styled-components'
 import App from './components/App/index.jsx'
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { configureStore } from './utils/redux/store';
 
 const root = document.getElementById('root')
+
+const store = configureStore();
+
+const persistor = persistStore(store);
 
 const Greet = styled.div`
   color: red;
 `
 
-render(<Greet><App /></Greet>, root)
+render(
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <Greet>
+        <App />
+      </Greet>
+    </PersistGate>
+  </Provider>,
+  root,
+);
