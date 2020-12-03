04b8fdb2181cbe3b78ac610877d48f5b099550b7
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-12-03 16:56:44 -0600

    CReate buttons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 /**
  * This is header component show user login or sign up option
  */
-import { HeaderContainer, LogoContainer, ButtonsContainer } from './styled'
+import { HeaderContainer, LogoContainer, ButtonsContainer, Button } from './styled'
 import Logo from '../../assets/Logo/index.jsx'
 
 const Header = () => {
@@ -11,7 +11,10 @@ const Header = () => {
       <LogoContainer>
         <Logo />
       </LogoContainer>
-      <ButtonsContainer>yes</ButtonsContainer>
+      <ButtonsContainer>
+        <Button>Sign in</Button>
+        <Button>Sign up</Button>
+      </ButtonsContainer>
     </HeaderContainer>
   )
 }
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -13,3 +13,28 @@ export const LogoContainer = styled.div`
 export const ButtonsContainer = styled.div`
 
 `
+export const Button = styled.button`
+	box-shadow: 3px 4px 0px 0px rgba(255, 255, 255, 5);
+	background:linear-gradient(to bottom, rgba(255, 255, 255, 0) 5%, rgba(255, 255, 255, 1) 100%);
+	background-color: whitesmoke;
+	border-radius:18px;
+	border:1px solid black;
+	display:inline-block;
+	cursor:pointer;
+	color:black;
+	font-family:Arial;
+	font-size:17px;
+	padding:7px 25px;
+	text-decoration:none;
+
+&:hover {
+	background:linear-gradient(to bottom, rgba(255, 255, 255, 1) 5%, rgba(0, 0, 0, 1) 100%);
+  box-shadow: 3px 4px 0px 0px rgba(255, 255, 255, .1);
+	color: whitesmoke;
+}
+&:active {
+	position:relative;
+	top:1px;
+}
+
+`
