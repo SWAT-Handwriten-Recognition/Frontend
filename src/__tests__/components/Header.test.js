135922191a8ffee80328770ca0e5e0de67b6818b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-12-10 12:59:04 -0600

    Prepare test to new implementation

--- src/__tests__/components/Header.test.js
+++ src/__tests__/components/Header.test.js
@@ -1,9 +1,26 @@
 import { shallow } from 'enzyme';
 import Header from '../../components/Header/index.jsx';
 import toJson from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { configureStore } from '../../utils/redux/store.js';
+import { HashRouter as Router } from 'react-router-dom'
+
+const store = configureStore();
+
+const persistor = persistStore(store);
 
 describe('<Header />', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(
+    <Provider store={store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <Router basename="/">
+          <Header />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
 
   it('is the Header rendering?', () => {
     expect(wrapper.exists()).toBe(true);
