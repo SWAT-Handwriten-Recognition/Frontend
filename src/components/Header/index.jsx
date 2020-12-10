a54f28257dd8138cd9e0c2ff29a833a75ec1c0ec
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-12-10 13:21:27 -0600

    Update header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,13 +4,28 @@
 import { HeaderContainer, LogoContainer, ButtonsContainer, Button } from './styled'
 import { useHistory } from 'react-router-dom'
 import Logo from '../../assets/Logo/index.jsx'
+import { connect } from 'react-redux';
+import { useEffect, useState } from 'react';
 
-const Header = ({ scroll, normal }) => {
+const Header = ({ scroll, normal, user }) => {
   const history = useHistory()
+  const [thereIsUser, setThereIsUser] = useState(false)
 
   const handleSignUp = () => history.push('/signup')
 
   const handleSignIn = () => history.push('/signin')
+
+  console.log(thereIsUser)
+
+  useEffect(() => {
+    if (user) {
+      if (Object.entries(user).length === 2) {
+        setThereIsUser(value => value = true)
+      } else {
+        setThereIsUser(value => value = false)
+      }
+    }
+  }, [user])
 
   return (
     <HeaderContainer normal={normal} scroll={scroll}>
@@ -25,4 +40,8 @@ const Header = ({ scroll, normal }) => {
   )
 }
 
-export default Header
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps)(Header);
