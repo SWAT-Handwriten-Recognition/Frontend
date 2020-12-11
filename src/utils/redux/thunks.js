c7d5513afbdc0f213a0486479d110babe8ee9ed6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-12-10 21:02:19 -0600

    mock get signs

--- src/utils/redux/thunks.js
+++ src/utils/redux/thunks.js
@@ -48,22 +48,7 @@ export const signinUser = (data) => async (dispatch) => {
 
 export const getSigns = ({ status, token }) => async (dispatch) => {
   try {
-    const response = await postData('https://api.jonastronco.xyz/signatures/', {
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': `${token}`,
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({ username: status.username }), // body data type must match "Content-Type" header
-    }).then((response) => dispatch(getSignatures(response)));
-
-    console.log(await response, token);
+    dispatch(getSignatures([{}]));
   } catch (error) {
     alert(error);
   }
