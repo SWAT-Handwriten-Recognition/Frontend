6811f8e2273e5e129b0a0b3fba954c21143cb880
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-12-10 18:16:06 -0600

    [UPDATE] try to make get data

--- src/utils/redux/thunks.js
+++ src/utils/redux/thunks.js
@@ -37,12 +37,10 @@ export const registerUser = (data) => async (dispatch) => {
 
 export const signinUser = (data) => async (dispatch) => {
   try {
-    const response = await postData(
+    await postData(
       'https://api.jonastronco.xyz/users/login/',
       data
     ).then((data) => dispatch(setUser(data)));
-
-    console.log(await response);
   } catch (error) {
     alert(error);
   }
@@ -51,14 +49,21 @@ export const signinUser = (data) => async (dispatch) => {
 export const getSigns = ({ status, token }) => async (dispatch) => {
   try {
     const response = await postData('https://api.jonastronco.xyz/signatures/', {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
       headers: {
-        Authorization: token,
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json',
+        'x-access-token': `${token}`,
+        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: token,
-    }).then(() => dispatch(getSignatures(status?.username)));
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({ username: status.username }), // body data type must match "Content-Type" header
+    }).then((response) => dispatch(getSignatures(response)));
 
-    console.log(await response);
+    console.log(await response, token);
   } catch (error) {
     alert(error);
   }
